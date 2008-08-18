a433744ece1d404aa92166355ec9a44b4a13125b
Sylvain Viollon <thefunny@gmail.com>
2008-08-18 12:42:32 +0000

    - Add a way to select interface/layer when you customize a template,
    - Improve the way interface/layer are searched.

--- silva/core/views/customization.py
+++ silva/core/views/customization.py
@@ -3,9 +3,8 @@
 # $Id$
 
 from zope.app.apidoc.presentation import getViews
-from zope.interface.interfaces import IInterface
-from zope.publisher.interfaces.browser import IBrowserRequest
-from zope import component
+from zope.publisher.interfaces.browser import IBrowserRequest, IDefaultBrowserLayer
+from zope.component import getUtility, getUtilitiesFor
 
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 from OFS.Folder import Folder
@@ -13,12 +12,14 @@ from OFS.Folder import Folder
 from Products.Silva.BaseService import SilvaService
 from Products.Silva.helpers import add_and_edit
 from Products.Silva import interfaces as silva_interfaces
+from Products.SilvaLayout.interfaces import ISilvaLayer, ISilvaSkin
 
 from silva.core.views.ttwtemplates import TTWViewTemplate
 
 from silva.core import conf as silvaconf
 from silva.core.views import views as silvaviews
-from silva.core.views.interfaces import ISilvaView
+
+from interfaces import ISilvaView, ISilvaCustomizableType, ISilvaLayerType
 
 
 class CustomizationService(Folder, SilvaService):
@@ -34,27 +35,58 @@ class CustomizationService(Folder, SilvaService):
         ) + Folder.manage_options
 
 
-class ManageCustomTemplates(silvaviews.ZMIView):
+class CustomizationManagementView(silvaviews.ZMIView):
 
     silvaconf.context(CustomizationService)
-    silvaconf.name('manage_customization')
     silvaconf.require('zope2.ViewManagementScreens')
+    silvaconf.baseclass()
+
+    interface = None
+    layer = None
+
+    def availablesInterfaces(self):
+        """This return available interfaces starting from base.
+        """
+        base = self.interface
+        if base is None:
+            base = silva_interfaces.ISilvaObject
+        interfaces = getUtilitiesFor(ISilvaCustomizableType)
+        return [name for name, interface in interfaces if interface.isOrExtends(base)]
+
+    def availablesLayers(self):
+        """This return available layers starting from base.
+        """
+        base = self.layer
+        if base is None:
+            base = IDefaultBrowserLayer
+        layers = getUtilitiesFor(ISilvaLayerType)
+        return [name for name, layer in layers if layer.isOrExtends(base)]
+
+
+class ManageCustomTemplates(CustomizationManagementView):
+
+    silvaconf.name('manage_customization')
 
     def update(self):
         self.availableTemplates = []
         self.selectedInterface = self.request.form.get('interface', None)
+        self.selectedLayer = self.request.form.get('layer', IDefaultBrowserLayer.__identifier__)
+
         if self.selectedInterface:
 
             templates = []
 
-            interface = component.getUtility(IInterface, name=self.selectedInterface)
+            interface = getUtility(ISilvaCustomizableType, name=self.selectedInterface)
+            layer = getUtility(ISilvaLayerType, name=self.selectedLayer)
+
             for reg in getViews(interface, IBrowserRequest):
                 if ((hasattr(reg.factory, '__name__') and reg.factory.__name__.startswith('SimpleViewClass')) or
                     ISilvaView.implementedBy(reg.factory)):
                     templates.append(reg)
 
             for reg in sorted(templates, key=lambda r: r.name):
-                if not reg.required[0].isOrExtends(silva_interfaces.ISilvaObject):
+                if not (reg.required[0].isOrExtends(silva_interfaces.ISilvaObject) and
+                        reg.required[1].isOrExtends(layer)):
                     continue
                 customizable = True
                 if ISilvaView.implementedBy(reg.factory):
@@ -75,33 +107,27 @@ class ManageCustomTemplates(silvaviews.ZMIView):
                         'config': config,
                         'customizable': customizable
                         })
-
-
-    def availablesInterfaces(self):
-        interfaces = component.getUtilitiesFor(IInterface)
-        return [name for name, interface in interfaces if interface.isOrExtends(silva_interfaces.ISilvaObject)]
+        else:
+            self.selectedInterface = silva_interfaces.ISilvaObject.__identifier__
         
 
-class ManageViewTemplate(silvaviews.ZMIView):
+class ManageViewTemplate(CustomizationManagementView):
 
-    silvaconf.context(CustomizationService)
     silvaconf.name('manage_template')
-    silvaconf.require('zope2.ViewManagementScreens')
 
     def update(self):
         assert 'for' in self.request.form
         assert 'name' in self.request.form
 
         self.name = self.request.form['name']
-        self.for_name = self.request.form['for']
-        self.layer_name = self.request.form['layer']
-        self.interface = component.getUtility(IInterface, name=self.for_name)
+        self.layer = getUtility(ISilvaLayerType, self.request.form['layer'])
+        self.interface = getUtility(ISilvaCustomizableType, name=self.request.form['for'])
 
         view = None
         for reg in getViews(self.interface, IBrowserRequest):
             if (reg.name == self.name and 
-                reg.required[0].__identifier__ == self.for_name and
-                reg.required[1].__identifier__ == self.layer_name):
+                reg.required[0] == self.interface and
+                reg.required[1] == self.layer):
                 view = reg
                 break
         if view is None:
@@ -113,6 +139,7 @@ class ManageViewTemplate(silvaviews.ZMIView):
             self.code = open(self.factory.template._template.filename, 'rb').read()
         else:
             self.code = open(self.factory.index.filename, 'rb').read()
+
 
 class ManageCreateCustomTemplate(ManageViewTemplate):
 
@@ -128,8 +155,13 @@ class ManageCreateCustomTemplate(ManageViewTemplate):
     def update(self):
         super(ManageCreateCustomTemplate, self).update()
 
-        template_id = '%s-%s-%s' % (self.for_name.split('.')[-1],
-                                    self.layer_name.split('.')[-1],
+        for_name = self.request.form['customize_for']
+        layer_name = self.request.form['customize_layer']
+        customize_for = getUtility(ISilvaCustomizableType, name=for_name)
+        customize_layer = getUtility(ISilvaLayerType, name=layer_name)
+        
+        template_id = '%s-%s-%s' % (for_name.split('.')[-1],
+                                    layer_name.split('.')[-1],
                                     self.name)
 
         viewclass = self.factory.__bases__[0]
@@ -141,7 +173,7 @@ class ManageCreateCustomTemplate(ManageViewTemplate):
         self.context._setObject(template_id, new_template)
 
         manager = self.context.get_root().getSiteManager()
-        manager.registerAdapter(new_template, required=self.reg.required, 
+        manager.registerAdapter(new_template, required=(customize_for, customize_layer,),
                                 provided=self.reg.provided, name=self.name)
 
         new_template = getattr(self.context, template_id)
--- silva/core/views/interfaces.py
+++ silva/core/views/interfaces.py
@@ -3,6 +3,7 @@
 # See also LICENSE.txt
 # $Id$
 
+from zope.interface.interfaces import IInterface
 from zope import interface
 from zope import schema
 
@@ -57,6 +58,23 @@ class ISilvaZ3CFormForm(ISilvaForm):
     """A Silva form built using z3c.form.
     """
 
+
+# customizable interfaces
+
+class ISilvaCustomizableType(IInterface):
+    """This type represent customizable interface in Silva. THIS IS
+    NOT AN INTERFACE TO PROVIDES OR IMPLEMENTS.
+    """
+
+class ISilvaLayerType(IInterface):
+    """This type represent Silva layer. THIS NOT AND INTERFACE TO
+    PROVIDES OR IMPLEMENTS.
+    """
+
+class ISilvaNotCustomizable(interface.Interface):
+    """Marker interface to put on view that you don't people be able
+    to customize.
+    """
 
 # z3c.form Silva support
 
