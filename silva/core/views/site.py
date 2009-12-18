65127adc4eff6a721e6fc57d8baec495344649c2
Sylvain Viollon <thefunny@gmail.com>
2009-12-18 12:01:16 +0000

    Port change from 2.1 to 2.2

--- silva/core/views/site.py
+++ silva/core/views/site.py
@@ -3,22 +3,41 @@
 # See also LICENSE.txt
 # $Id$
 
-from Products.Five import BrowserView
+from five import grok
+from zope.app.component.hooks import getSite
+from zope.publisher.interfaces.http import IHTTPRequest
+from zope.traversing.browser import absoluteURL
 
-class VirtualSite(BrowserView):
+from silva.core.views.interfaces import IVirtualSite
+
+
+class VirtualSite(grok.Adapter):
+    grok.implements(IVirtualSite)
+    grok.provides(IVirtualSite)
+    grok.context(IHTTPRequest)
+
+    def __init__(self, request):
+        self.request = request
 
     def get_root(self):
         root = self.get_virtual_root()
         if root is None:
-            return self.context.get_root()
+            return self.get_silva_root()
         return root
+
+    def get_root_url(self):
+        return absoluteURL(self.get_root(), self.request)
+
+    def get_silva_root(self):
+        # XXX Check for nested localsites in Silva
+        return getSite()
 
     def get_virtual_root(self):
         root_path = self.get_virtual_path()
         if root_path is None:
             return None
 
-        return self.context.restrictedTraverse(root_path, None)
+        return getSite().restrictedTraverse(root_path, None)
 
     def get_virtual_path(self):
         try:
