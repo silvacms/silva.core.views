37fc99b3faa8d804d28c077d15b01d090b59ec08
Sylvain Viollon <thefunny@gmail.com>
2010-03-23 13:50:41 +0000

    Prevent a bug in Zope 2.12.

--- silva/core/views/traverser.py
+++ silva/core/views/traverser.py
@@ -1,17 +1,34 @@
 # Copyright (c) 2002-2010 Infrae. All rights reserved.
 # See also LICENSE.txt
 # $Id$
-from zope.interface import alsoProvides
+from zope.interface import alsoProvides, implements
 from zope.component import getMultiAdapter
 from zope.publisher.interfaces.browser import IBrowserRequest
 from zope.traversing.interfaces import ITraversable
 from silva.core.views.interfaces import IHTTPResponseHeaders
 from five import grok
 
+from Acquisition.interfaces import IAcquirer
 from ZPublisher.BaseRequest import DefaultPublishTraverse
 from silva.core.interfaces import IPublication
 
 from silva.core.views.interfaces import IPreviewLayer
+
+
+class UseParentByAcquisition(object):
+    """Request traverseName which query the ITraversable adapter always
+    do a __of__ of the result of the queried adapter to get the new
+    context object to use afterward. It is annoying if you don't want
+    to change of context object, as if you return the same object you
+    got in input it will have __of__ of himself.
+
+    Returning an instance of this object will prevent that behavior
+    and permit you to stay on the same context object.
+    """
+    implements(IAcquirer)
+
+    def __of__(self, obj):
+        return obj
 
 
 class PreviewTraversable(grok.MultiAdapter):
@@ -30,7 +47,7 @@ class PreviewTraversable(grok.MultiAdapter):
     def traverse(self, name, remaining):
         if not IPreviewLayer.providedBy(self.request):
             alsoProvides(self.request, IPreviewLayer)
-        return self.context
+        return UseParentByAcquisition()
 
 
 class SilvaPublishTraverse(DefaultPublishTraverse):
