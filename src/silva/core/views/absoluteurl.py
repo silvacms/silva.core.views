40b187873d0f61ea2adb9bf38cda340608a819fb
Sylvain Viollon <thefunny@gmail.com>
2012-06-14 17:37:04 +0200

    Make absolute url buisness more generic.

--- src/silva/core/views/absoluteurl.py
+++ src/silva/core/views/absoluteurl.py
@@ -3,8 +3,8 @@
 # $Id$
 
 # Zope 3
-from zope.component import queryMultiAdapter
-from zope.interface import implements
+from five import grok
+from zope.component import queryMultiAdapter, getMultiAdapter
 
 # Zope 2
 from Products.Five import BrowserView
@@ -29,62 +29,18 @@ class AbsoluteURL(BrowserView):
 
     (original: zope.traversing.browser.absoluteurl)
     """
-    implements(IContentURL)
+    grok.implements(IContentURL)
 
     def __init__(self, context, request):
         self.context = context
         self.request = request
 
-    def is_virtual_root(self, path):
-        virtual_path = self.request.physicalPathToVirtualPath(path)
-        return not virtual_path
-
-    def title(self, preview=False):
-        if preview:
-            return self.context.get_previewable().get_short_title()
-        return self.context.get_short_title()
-
-    def ancestors(self, preview=False, skip=None):
-        name = minimize(self.title(preview=preview))
-        container = aq_parent(self.context)
-        if skip is not None:
-            while skip.providedBy(container):
-                container = aq_parent(container)
-
-        if (container is None or
-            IRoot.providedBy(self.context) or
-            self.is_virtual_root(self.context.getPhysicalPath())):
-            return ({'name': name, 'url': self.__str__()},)
-
-        base = tuple()
-        parent = queryMultiAdapter((container, self.request), IContentURL)
-        if parent is not None:
-            base = tuple(parent.ancestors(preview, skip))
-
-        if (not IDisableBreadcrumbTag.providedBy(self.context) and
-            (not IContent.providedBy(self.context) or
-             not self.context.is_default())):
-            base += ({'name': name, 'url': self.__str__()},)
-
-        return base
+    def _url(self, path, preview=False, relative=False):
+        return self.request.physicalPathToURL(path, relative=relative)
 
     def url(self, preview=False, relative=False):
-        # Insert back the preview namespace. Maybe there is a better
-        # way to do it, but have to do it by hand here since
-        # ZPublisher.Request doesn't implements its interfaces
-        # properly.
         path = self.context.getPhysicalPath()
-        request = self.request
-
-        if preview is True:
-            # Handle preview.
-            root = self.context.get_root()
-            root_path = root.getPhysicalPath()
-            virtual_path = request.other.get('VirtualRootPhysicalPath', ('',))
-            preview_pos = max(len(root_path), len(virtual_path))
-            path = list(path)
-            path.insert(preview_pos, "++preview++")
-        return request.physicalPathToURL(path, relative=relative)
+        return self._url(path, preview=preview, relative=relative)
 
     def preview(self):
         return self.url(preview=True)
@@ -95,26 +51,92 @@ class AbsoluteURL(BrowserView):
     __call__ = __repr__ = __unicode__ = __str__
 
     def breadcrumbs(self):
-        return self.ancestors(
+        return tuple()
+
+
+class BrainAbsoluteURL(AbsoluteURL):
+    """ContentURL for brains.
+    """
+
+    def url(self, preview=False, relative=False):
+        path = self.context.getPath().rstrip('/').split('/')
+        return self._url(path, preview=preview, relative=relative)
+
+
+class ContentAbsoluteURL(AbsoluteURL):
+    """ContentURL for Silva objects
+    """
+
+    def _virtual_root(self, path):
+        virtual_path = self.request.physicalPathToVirtualPath(path)
+        return not virtual_path
+
+    def _title(self, preview=False):
+        if preview:
+            return self.context.get_previewable().get_short_title()
+        return self.context.get_short_title()
+
+    def _url(self, path, preview=False, relative=False):
+        # Insert back the preview namespace. Maybe there is a better
+        # way to do it, but have to do it by hand here since
+        # ZPublisher.Request doesn't implements its interfaces
+        # properly.
+        request = self.request
+
+        if preview is True:
+            root = self.context.get_root()
+            root_path = root.getPhysicalPath()
+            virtual_path = request.other.get('VirtualRootPhysicalPath', ('',))
+            preview_pos = max(len(root_path), len(virtual_path))
+            path = list(path)
+            path.insert(preview_pos, "++preview++")
+        return request.physicalPathToURL(path, relative=relative)
+
+    def _breadcrumbs(self, preview=False, skip=None):
+        name = minimize(self._title(preview=preview))
+        container = aq_parent(self.context)
+        if skip is not None:
+            while skip.providedBy(container):
+                container = aq_parent(container)
+
+        if (container is None or
+            IRoot.providedBy(self.context) or
+            self._virtual_root(self.context.getPhysicalPath())):
+            return ({'name': name, 'url': self.__str__()},)
+
+        base = tuple()
+        parent = queryMultiAdapter((container, self.request), IContentURL)
+        if parent is not None:
+            base = tuple(parent._breadcrumbs(preview, skip))
+
+        if (not IDisableBreadcrumbTag.providedBy(self.context) and
+            (not IContent.providedBy(self.context) or
+             not self.context.is_default())):
+            base += ({'name': name, 'url': self.__str__()},)
+
+        return base
+
+    def breadcrumbs(self):
+        return self._breadcrumbs(
             skip=None,
             preview=IPreviewLayer.providedBy(self.request))
 
 
-class VersionAbsoluteURL(AbsoluteURL):
-    """AbsoluteURL for a version
+class VersionAbsoluteURL(ContentAbsoluteURL):
+    """ContentURL for a Silva version.
     """
 
-    def title(self, preview=False):
+    def _title(self, preview=False):
         return self.context.get_short_title()
 
     def breadcrumbs(self):
-        return self.ancestors(
+        return self._breadcrumbs(
             skip=IVersionedContent,
             preview=IPreviewLayer.providedBy(self.request))
 
 
-class ErrorAbsoluteURL(AbsoluteURL):
-    """AbsoluteURL for a version
+class ErrorAbsoluteURL(ContentAbsoluteURL):
+    """ContentURL for an error.
     """
 
     def __init__(self, context, request):
@@ -123,15 +145,10 @@ class ErrorAbsoluteURL(AbsoluteURL):
         self.request = request
 
 
-class TestAbsoluteURL(BrowserView):
+class TestAbsoluteURL(ContentAbsoluteURL):
     """An absolute URL provider for TestRequest. This is mainly to get
     test working.
     """
-    implements(IContentURL)
-
-    def __init__(self, context, request):
-        self.context = context
-        self.request = request
 
     def url(self, preview=False, relative=False):
         is_root = True
@@ -148,13 +165,12 @@ class TestAbsoluteURL(BrowserView):
             return url
         return u'http://localhost' + url
 
-    def preview(self):
-        return self.url(preview=True)
 
-    def __str__(self):
-        return self.url()
 
-    __call__ = __repr__ = __unicode__ = __str__
+def absolute_url(self, relative=0):
+    return getMultiAdapter(
+        (self, self.REQUEST), IContentURL).url(relative=bool(relative))
 
-    def breadcrumbs(self):
-        return tuple()
+def absolute_url_path(self):
+    return getMultiAdapter(
+        (self, self.REQUEST), IContentURL).url(relative=True)
